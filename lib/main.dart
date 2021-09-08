ed9bd035b2b753c2d1e3ed25f9b7c2060cbb6ee6
Utsabbasyal12 <utsabbasyal12@gmail.com>
2021-09-08 22:56:14 +0545

    Added app bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+      title: Text("I AM POOR"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ),
 
   ));
 }
